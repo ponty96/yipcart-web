55c5cf289d252f3df7d887519f57a7d2f7898319
Opeyemi Badmos <opeyemi.badmos@gmail.com>
2018-12-26 16:33:57 +0100

    change receiver's email

--- lib/yipcart/mailer/email.ex
+++ lib/yipcart/mailer/email.ex
@@ -5,7 +5,7 @@ defmodule Yipcart.Email do
 	def send_email(receipient, business_name) do
 		new_email(
 			from: receipient,
-      to: "opeyemi.badmos@yipcart.com",
+      to: "ayo.aregbede@yipcart.com",
       subject: "Waiting Customer",
 			text_body: "#{receipient} with the business name #{String.capitalize(business_name)} has been enlisted in the waiting list."
 		)
