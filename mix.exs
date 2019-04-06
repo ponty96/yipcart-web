7604e10e03516aa4d560439758dd32e95459017e
Opeyemi Badmos <opeyemi.badmos@gmail.com>
2019-04-06 21:38:18 +0100

    remove :bamboo dependency

--- mix.exs
+++ mix.exs
@@ -41,8 +41,7 @@ defmodule Yipcart.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
-      {:bamboo, "~> 1.1"}
+      {:cowboy, "~> 1.0"}
     ]
   end
 
